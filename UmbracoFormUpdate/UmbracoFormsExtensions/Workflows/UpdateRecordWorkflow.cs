c737ca816bbea5b2b1439b0c9880782cca4ca07f
Migaroez <geusens@gmail.com>
2021-12-13 15:36:01 +0100

    Added reproduced issue

---
+++ UmbracoFormUpdate/UmbracoFormsExtensions/Workflows/UpdateRecordWorkflow.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using umbraco.BusinessLogic;
+using Umbraco.Core;
+using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Enums;
+using Umbraco.Forms.Data.Storage;
+using Umbraco.Forms.Web.Services;
+
+/// <summary>
+/// Summary description for TestWorkflow
+/// </summary>
+public class UpdateRecordWorkflow : WorkflowType
+{
+    public UpdateRecordWorkflow()
+    {
+        this.Id = new Guid("83B6E13A-F704-4813-94A2-A8CCEE2DCE0D");
+        this.Name = "Update Record Test";
+        this.Description = "Tries to update the record without modifying";
+        this.Icon = "icon-chat-active";
+        this.Group = "Services";
+    }
+    public override WorkflowExecutionStatus Execute(Record record, RecordEventArgs e)
+    {
+        RecordStorage store = new RecordStorage();
+        store.UpdateRecord(record, e.Form);
+        store.Dispose();
+
+        return WorkflowExecutionStatus.Completed;
+    }
+
+    public override List<Exception> ValidateSettings()
+    {
+        return new List<Exception>();
+    }
+}
